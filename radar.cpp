07019fe7b5bac6534eb82633f6076927f22d7d7f
Gagan Bhogal <94851044+gigisbb@users.noreply.github.com>
2024-04-07 17:49:39 -0400

    Update radar.cpp

--- radar.cpp
+++ radar.cpp
@@ -1,129 +1,116 @@
-#include "Radar.h"
+#include "radar.h"
 #include <iostream>
-#include <cmath>
-#include <sys/netmgr.h>
-#include <sys/neutrino.h>
+#include <unistd.h>
+using namespace std;
 
-using std::cerr;
-using std::cout;
-using std::endl;
-using std::vector;
-
-Radar::Radar() = default;
-
-Radar::Radar(const std::vector<Plane>& planes) : planes(planes) {}
-
-Radar::~Radar() = default;
-
-// to collect data for all planes monitored by radar
-std::vector<std::pair<int, AircraftPositionResponse>> Radar::collectPlaneData() {
-    std::vector<std::pair<int, AircraftPositionResponse>> planeData;
-    for (const auto& plane : planes) {
-        // signal plane and get its response
-        AircraftPositionResponse response = signalPlane(plane);
-        // store response along with plane's flight ID
-        planeData.emplace_back(plane.getFlightId(), response);
+Radar::Radar() : server_coid(-1) {
+	// create new thread for radar routine
+    if (pthread_create(&thread_id, NULL, radarRoutine, (void*)this) != 0) {
+        std::cerr << "Radar: Failed to start radar tracking thread." << std::endl;
     }
-    return planeData;
 }
 
-// collect data from a specific plane
-bool Radar::pingPlane(int flightID, AircraftPositionResponse* out) {
-    // check if plane's flight ID matches requested ID
-    for (const auto& plane : planes) {
-        if (plane.getFlightId() == flightID) {
-            // signal plane and get its response
-            *out = signalPlane(plane);
-            return true;
-        }
-    }
-    // if requested id is not found
-    return false;
+Radar::~Radar() {
+	// signal thread to stop running
+	running = false;
+    // wait for thread to finish executing
+    pthread_join(thread_id, NULL);
 }
 
-// collect data for a specific subset of planes and get their responses
-std::vector<AircraftPositionResponse> Radar::pingMultiplePlanes(const std::vector<Plane>& selectedPlanes) {
-    std::vector<AircraftPositionResponse> responses;
-    for (const auto& plane : selectedPlanes) {
-        // signal each plane to get its response
-        responses.push_back(signalPlane(plane));
+// start tracking objects to get info about airspace objects
+void Radar::startTracking() {
+    while (true) {
+        getAirspace();
+        // radar checks airspace every 1sec
+        sleep(1);
     }
-    return responses;
 }
 
-// signal one plane and get its response
-AircraftPositionResponse Radar::signalPlane(const Plane& plane) {
-    AircraftPositionResponse response;
-    // connect to the plane's channel
-    int connectionID = ConnectAttach(0, 0, plane.getChannelID(), _NTO_SIDE_CHANNEL, 0);
-    // check if connection is successful
-    if (connectionID != -1) {
-        // create a message to signal the plane
-        PlaneCommandMessage msg = {COMMAND_RADAR_SIGNAL};
-        // send the message and receive its response
-        if (MsgSend(connectionID, &msg, sizeof(msg), &response, sizeof(response)) != -1) {
-            // get responses for position, FL, and speed
-            response.x = plane.getXPosition();
-            response.y = plane.getYPosition();
-            response.z = plane.getZPosition();
-            response.flightLevel = plane.getFlightLevel();
-            response.speedX = plane.getXSpeed();
-            response.speedY = plane.getYSpeed();
-            response.speedZ = plane.getZSpeed();
+void* Radar::radarRoutine(void* arg) {
+	// thread routine to start tracking
+    Radar* radar = static_cast<Radar*>(arg);
+    radar->startTracking();
+    return NULL;
+}
+
+
+int Radar::toComputerSys(compsys_msg data) {
+    int server_coid = name_open(COMPUTER_SYSTEM_ATTACH_POINT, 0);
+    // try to connect with server
+    if (server_coid == -1) {
+        cerr << "Radar: Failed to connect with server\n\n";
+        return EXIT_FAILURE;
+    }
+    // try to send message to computer system
+    if (MsgSend(server_coid, &data, sizeof(data), nullptr, 0) == -1) {
+        cerr << "Radar: Failed to send message\n\n";
+        name_close(server_coid);
+        return EXIT_FAILURE;
+    }
+    name_close(server_coid);
+    return EXIT_SUCCESS;
+}
+
+void Radar::getAirspace() {
+    while (true) {
+    	sleep(1);
+        cout << "Radar: Timer waited 1s" << std::endl;
+        airspace = Plane::airspace;
+
+        if (airspace.empty()) {
+            cout << "Radar: Airspace empty" << std::endl;
         } else {
-            cerr << "Failed to send signal to the plane" << endl;
-            response.x = response.y = response.z = 0.0;
-            response.flightLevel = 0;
-            response.speedX = response.speedY = response.speedZ = 0;
-        }
-        // detach from channel
-        ConnectDetach(connectionID);
-    } else {
-        cerr << "Failed to attach to plane's channel" << endl;
-        response.x = response.y = response.z = 0.0;
-        response.flightLevel = 0;
-        response.speedX = response.speedY = response.speedZ = 0;
-    }
-    return response;
-}
-
-// collect and send data for all planes monitored by radar
-void Radar::collectAndSendAircraftData() {
-    for (const auto& plane : planes) {
-        AircraftPositionResponse response = signalPlane(plane);
-
-        AircraftData data;
-        data.flightId = plane.getFlightId();
-        data.x = response.x;
-        data.y = response.y;
-        data.z = response.z;
-        data.flightLevel = response.flightLevel;
-        data.speedX = response.speedX;
-        data.speedY = response.speedY;
-        data.speedZ = response.speedZ;
-
-        // send aircraft data
-        if (!sendAircraftData(data)) {
-
-            cerr << "Failed to send aircraft data for plane with ID: " << plane.getFlightId() << endl;
+            processAirspace(airspace);
         }
     }
 }
 
-// send aircraft data via message passing to computer system
-bool Radar::sendAircraftData(const AircraftData& data) {
-    int connectionID = ConnectAttach(0, 0, CHANNEL_ID, _NTO_SIDE_CHANNEL, 0);
-    if (connectionID != -1) {
-        if (MsgSend(connectionID, &data, sizeof(data), NULL, 0) != -1) {
-            ConnectDetach(connectionID);
-            return true;
-        } else {
-            cerr << "Failed to send aircraft data" << endl;
-            ConnectDetach(connectionID);
-            return false;
+void Radar::processAirspace(const std::vector<int>& airspace) {
+    compsys_msg data;
+    data.hdr.type = 0x01;
+
+    // iterate over each element in airspace
+    for (int planeId : airspace) {
+        try {
+            auto planeData = getPlaneData(planeId);
+            allPlaneData.push_back(planeData);
+        } catch (const exception& e) {
+        	// if error to process plane get next id and exception description
+            cerr << "Radar: Failed to process plane " << planeId << ": " << e.what() << std::endl;
+            continue;
         }
-    } else {
-        cerr << "Failed to attach to computer system channel" << endl;
-        return false;
+    }
+    // send data to compsys and clear collected data for next
+    // data gathering cycle
+    if (!allPlaneData.empty()) {
+        data.allPlanes = allPlaneData;
+        toComputerSys(data);
+        allPlaneData.clear();
     }
 }
+
+planes_information Radar::getPlaneData(int planeId) {
+	// store plane data
+    planes_information planeMsg;
+
+    // connect to server using plane id
+    // convert id to string then to c string
+    int server_coid = name_open(to_string(planeId).c_str(), 0);
+    if (server_coid == -1) {
+        throw runtime_error("Radar: Error occurred while attaching to channel");
+    }
+
+    msg pos_msg;
+    pos_msg.hdr.type = 0x00; //
+
+    if (MsgSend(server_coid, &pos_msg, sizeof(pos_msg), &planeMsg, sizeof(planeMsg)) == -1) {
+        name_close(server_coid);
+        throw std::runtime_error("Error occurred while sending message");
+    }
+
+    name_close(server_coid);
+    cout << "Radar: Data of Plane #" << planeMsg.ID << ": Coords(" << planeMsg.arrivalPosX << ", " << planeMsg.arrivalPosY << ", " << planeMsg.arrivalPosZ << ")" << endl;
+
+    return planeMsg;
+}
+
